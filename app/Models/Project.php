b94ecdaeec53ddc751d8dbd5756be566c2444620
Bruno Canini <brunocanini1994@gmail.com>
2023-07-12 15:56:54 +0200

    feat tabella ponte

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -22,4 +22,8 @@ class Project extends Model
     public function category() {
         return $this->belongsTo(Category::class);
     }
+
+    public function technologys() {
+        return $this->belongsToMany(Technology::class);
+    }
 }
---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Technology;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 50; $i++) { 
+            $technology = new Technology();
+            $technology->name = "#" . $faker->word();
+            $technology->save();
+        }
+    }
+}
